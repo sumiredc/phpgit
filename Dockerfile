6810caa140e3ecfc28b8776dfd2ceb027929efa1
Ryosuke Sakamoto <sakamoto@sumire-sakamoto.co.jp>
2025-04-12 10:36:58 +0900

    first test

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,10 @@
-FROM php:8.4.5-cli-bullseye
+FROM php:8.4.6-cli-bullseye
 
 WORKDIR /app
+
+RUN pecl install xdebug \
+    && docker-php-ext-enable xdebug
+
+COPY php.ini "$PHP_INI_DIR/php.ini"
 
 ENTRYPOINT [ "php", "src/app.php" ]
---
+++ tests/Unit/Domain/FileStatTest.php
@@ -0,0 +1,42 @@
+<?php
+
+use Phpgit\Domain\FileStat;
+
+describe('make', function () {
+    it('should match args to properties', function (array $stat) {
+        $fileStat = FileStat::make($stat);
+
+        expect($fileStat->dev)->toBe($stat['dev']);
+        expect($fileStat->ino)->toBe($stat['ino']);
+        expect($fileStat->mode)->toBe($stat['mode']);
+        expect($fileStat->nlink)->toBe($stat['nlink']);
+        expect($fileStat->uid)->toBe($stat['uid']);
+        expect($fileStat->gid)->toBe($stat['gid']);
+        expect($fileStat->rdev)->toBe($stat['rdev']);
+        expect($fileStat->size)->toBe($stat['size']);
+        expect($fileStat->atime)->toBe($stat['atime']);
+        expect($fileStat->mtime)->toBe($stat['mtime']);
+        expect($fileStat->ctime)->toBe($stat['ctime']);
+        expect($fileStat->blksize)->toBe($stat['blksize']);
+        expect($fileStat->blocks)->toBe($stat['blocks']);
+    })
+        ->with([
+            [
+                [
+                    'dev' => 10,
+                    'ino' => 11,
+                    'mode' => 12,
+                    'nlink' => 13,
+                    'uid' => 14,
+                    'gid' => 15,
+                    'rdev' => 16,
+                    'size' => 17,
+                    'atime' => 18,
+                    'mtime' => 19,
+                    'ctime' => 20,
+                    'blksize' => 21,
+                    'blocks' => 22,
+                ]
+            ]
+        ]);
+});
