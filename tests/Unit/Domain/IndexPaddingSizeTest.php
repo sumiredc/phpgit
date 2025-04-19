d1a8ff8a685477e6079b1f90aab604b202147605
Ryosuke Sakamoto <sakamoto@sumire-sakamoto.co.jp>
2025-04-20 04:39:34 +0900

    test: IndexPaddingSizeTest

---
+++ tests/Unit/Domain/IndexPaddingSizeTest.php
@@ -0,0 +1,67 @@
+<?php
+
+declare(strict_types=1);
+
+use Phpgit\Domain\IndexEntryPathSize;
+use Phpgit\Domain\IndexEntrySize;
+use Phpgit\Domain\IndexPaddingSize;
+
+describe('new', function () {
+    it('should match to properties', function (string $path, int $expected) {
+        $size = IndexEntrySize::new(IndexEntryPathSize::new($path));
+        $actual = IndexPaddingSize::new($size);
+
+        expect($actual->value)->toBe($expected);
+    })
+        ->with([
+            [str_repeat('a', 9), 6],
+            [str_repeat('a', 8), 7],
+            [str_repeat('a', 7), 0],
+            [str_repeat('a', 6), 1],
+            [str_repeat('a', 5), 2],
+            [str_repeat('a', 4), 3],
+            [str_repeat('a', 3), 4],
+            [str_repeat('a', 2), 5],
+            [str_repeat('a', 1), 6],
+        ]);
+});
+
+describe('isEmpty', function () {
+    it('should match to size zero', function (string $path, bool $expected) {
+        $size = IndexEntrySize::new(IndexEntryPathSize::new($path));
+        $actual = IndexPaddingSize::new($size);
+
+        expect($actual->isEmpty())->toBe($expected);
+    })
+        ->with([
+            [str_repeat('a', 9), false],
+            [str_repeat('a', 8), false],
+            [str_repeat('a', 7), true],
+            [str_repeat('a', 6), false],
+            [str_repeat('a', 5), false],
+            [str_repeat('a', 4), false],
+            [str_repeat('a', 3), false],
+            [str_repeat('a', 2), false],
+            [str_repeat('a', 1), false],
+        ]);
+});
+
+describe('asPadding', function () {
+    it('should match to padding string', function (string $path, string $expected) {
+        $size = IndexEntrySize::new(IndexEntryPathSize::new($path));
+        $actual = IndexPaddingSize::new($size);
+
+        expect($actual->asPadding())->toBe($expected);
+    })
+        ->with([
+            [str_repeat('a', 9), "\0\0\0\0\0\0"],
+            [str_repeat('a', 8), "\0\0\0\0\0\0\0"],
+            [str_repeat('a', 7), ''],
+            [str_repeat('a', 6), "\0"],
+            [str_repeat('a', 5), "\0\0"],
+            [str_repeat('a', 4), "\0\0\0"],
+            [str_repeat('a', 3), "\0\0\0\0"],
+            [str_repeat('a', 2), "\0\0\0\0\0"],
+            [str_repeat('a', 1), "\0\0\0\0\0\0"],
+        ]);
+});
