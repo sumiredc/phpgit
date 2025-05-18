20d78b60537d98f7f8e88495fa097cd722050aa3
Ryosuke Sakamoto <sakamoto@sumire-sakamoto.co.jp>
2025-05-18 15:32:27 +0900

    feat: global throw function

---
+++ functions/throw.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * @template R
+ * @template T of Throwable
+ * 
+ * @param callable(): R $callback
+ * @param ?class-string<T> $replaceException
+ * @return R
+ * @throws T
+ * @throws InvalidArgumentException
+ */
+
+function try_or_throw(callable $callback, ?string $replaceException = null)
+{
+    if (
+        !is_null($replaceException)
+        && !is_subclass_of($replaceException, Throwable::class)
+    ) {
+        throw new InvalidArgumentException(sprintf('Not allows specifies exceptions: $s', $replaceException));
+    }
+
+    try {
+        return $callback();
+    } catch (Throwable $th) {
+        throw is_null($replaceException)
+            ? $th
+            : new $replaceException($th->getMessage(), $th->getCode(), $th->getPrevious());
+    }
+}
+
+/**
+ * @template T of Throwable
+ * 
+ * @param T $th
+ * @throws T
+ */
+function throw_if(bool $condition, Throwable $th): void
+{
+    if ($condition) {
+        throw $th;
+    }
+}
+
+/**
+ * @template T of Throwable
+ * 
+ * @param T $th
+ * @throws T
+ */
+function throw_unless(bool $condition, Throwable $th): void
+{
+    if ($condition) {
+        return;
+    }
+
+    throw $th;
+}
