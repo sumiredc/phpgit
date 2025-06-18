188d09104a2f582760b34d1bc048046cf2673211
Ryosuke Sakamoto <sakamoto@sumire-sakamoto.co.jp>
2025-06-19 03:15:27 +0900

    add feature test

--- tests/Feature/src/Infra/Printer/CliPrinterTest.php
+++ tests/Feature/src/Infra/Printer/CliPrinterTest.php
@@ -3,45 +3,85 @@
 declare(strict_types=1);
 
 use Phpgit\Infra\Printer\CliPrinter;
+use Symfony\Component\Console\Formatter\OutputFormatter;
 use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Output\BufferedOutput;
 use Symfony\Component\Console\Output\NullOutput;
 
 describe('stackTrace', function () {
-    it('should match to output console stack trace', function (Throwable $th) {
-        $input = new ArrayInput([]);
-        $output = new BufferedOutput(BufferedOutput::VERBOSITY_NORMAL, false);
+    it(
+        'matches to output console stack trace',
+        function (Throwable $th) {
+            $input = new ArrayInput([]);
+            $output = new BufferedOutput(BufferedOutput::VERBOSITY_NORMAL, false);
 
-        $printer = new CliPrinter($input, $output);
-        $printer->stackTrace($th);
-        $actual = $output->fetch();
+            $printer = new CliPrinter($input, $output);
+            $printer->stackTrace($th);
+            $actual = $output->fetch();
 
-        expect($actual)->toContain('[ERROR]');
-        expect($actual)->toContain($th->getMessage());
-        expect($actual)->toContain($th->getFile());
-    })
+            expect($actual)->toContain('[ERROR]');
+            expect($actual)->toContain($th->getMessage());
+            expect($actual)->toContain($th->getFile());
+        }
+    )
         ->with([
             [new RuntimeException],
             [new InvalidArgumentException],
         ]);
 });
 
-
 describe('echo', function () {
-    it('should match to output console string', function (string $message) {
-        $input = new ArrayInput([]);
-        $output = new NullOutput();
+    it(
+        'matches to output console string',
+        function (string $message) {
+            $input = new ArrayInput([]);
+            $output = new NullOutput();
 
-        $printer = new CliPrinter($input, $output);
+            $printer = new CliPrinter($input, $output);
 
-        ob_start();
-        $printer->echo($message);
-        $actual = ob_get_clean();
+            ob_start();
+            $printer->echo($message);
+            $actual = ob_get_clean();
 
-        expect($actual)->toBe($message);
-    })
+            expect($actual)->toBe($message);
+        }
+    )
         ->with([
             ['Initialized empty Git repository in phpgit'],
             ['Reinitialized empty Git repository in phpgit']
+        ]);
+});
+
+describe('writelnDiffStat', function () {
+    it(
+        'matches to output console string',
+        function (
+            int $pathLength,
+            int $diffDigits,
+            string $path,
+            int $insertions,
+            int $deletions,
+            string $expected,
+        ) {
+            $input = new ArrayInput([]);
+            $output = new BufferedOutput(BufferedOutput::VERBOSITY_NORMAL, false);
+
+            $printer = new CliPrinter($input, $output);
+
+            $printer->writelnDiffStat($pathLength, $diffDigits, $path, $insertions, $deletions);
+            $actual = $output->fetch();
+
+            expect($actual)->toBe($expected);
+        }
+    )
+        ->with([
+            [
+                'pathLength' => 15,
+                'diffDigits' => 3,
+                'path' => 'dummy-path',
+                'insertions' => 6,
+                'deletions' => 12,
+                'expected' => " dummy-path      |  18 \e[32m++++++\e[39m\e[31m------------\e[39m\n",
+            ],
         ]);
 });
