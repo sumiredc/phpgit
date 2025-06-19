b829fe56e0f9432ef395d666c0944197904aaeee
Ryosuke Sakamoto <sakamoto@sumire-sakamoto.co.jp>
2025-06-19 22:32:03 +0900

    add comment in TreeEntry

--- src/Domain/TreeEntry.php
+++ src/Domain/TreeEntry.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Phpgit\Domain;
 
+use InvalidArgumentException;
+
 readonly final class TreeEntry
 {
     private function __construct(
@@ -13,12 +15,19 @@ readonly final class TreeEntry
         public readonly ObjectHash $objectHash
     ) {}
 
+    /**
+     * @throws InvalidArgumentException
+     */
     public static function new(
         ObjectType $objectType,
         GitFileMode $gitFileMode,
         string $objectName,
         ObjectHash $objectHash
     ): self {
-        return new self($objectType, $gitFileMode, $objectName, $objectHash);
+        if (in_array($objectType, [ObjectType::Blob, ObjectType::Tree], true)) {
+            return new self($objectType, $gitFileMode, $objectName, $objectHash);
+        }
+
+        throw new InvalidArgumentException(sprintf('not allowed ObjectType: %s', $objectType->name));
     }
 }
