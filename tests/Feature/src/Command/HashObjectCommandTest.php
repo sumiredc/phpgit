06655ac502cdf8d1f5dfc980c425453533c34e8e
Ryosuke Sakamoto <sakamoto@sumire-sakamoto.co.jp>
2025-07-06 22:11:00 +0900

    test: HashObjectCommandTest

---
+++ tests/Feature/src/Command/HashObjectCommandTest.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+use Phpgit\Command\HashObjectCommand;
+use Symfony\Component\Console\Tester\CommandTester;
+use Tests\CommandRunner;
+
+beforeAll(function () {
+    CommandRunner::run('php git init');
+});
+
+describe('commit-tree', function () {
+    it(
+        'outputs commit-tree results',
+        function () {
+            file_put_contents(F_GIT_TRACKING_ROOT . '/README.md', "Hello, World!\n");
+
+            $tester = new CommandTester(new HashObjectCommand);
+            $tester->execute([
+                'file' => 'README.md',
+            ]);
+
+            $output = $tester->getDisplay();
+            $exitCode = $tester->getStatusCode();
+
+            expect($output)->toBe("8ab686eafeb1f44702738c8b0f24f2567c36da6d\n");
+            expect($exitCode)->toBeExitSuccess();
+        }
+    );
+});
