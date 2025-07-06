fe8242eaa22adc113353367c1f4b8f1fdeb5e2c0
Ryosuke Sakamoto <sakamoto@sumire-sakamoto.co.jp>
2025-07-06 21:51:55 +0900

    fix: InitCommandTest expected value

--- tests/Feature/src/Command/InitCommandTest.php
+++ tests/Feature/src/Command/InitCommandTest.php
@@ -4,23 +4,39 @@ declare(strict_types=1);
 
 use Phpgit\Command\InitCommand;
 use Symfony\Component\Console\Tester\CommandTester;
+use Tests\CommandRunner;
+
+beforeEach(function () {
+    CommandRunner::run('rm -rf /tmp/project/.git');
+});
 
 describe('init', function () {
     it(
         'outputs initialize message',
-        function (string $output) {
+        function () {
             $tester = new CommandTester(new InitCommand);
             $tester->execute([]);
 
             $output = $tester->getDisplay();
             $exitCode = $tester->getStatusCode();
 
-            expect($output)->toBe($output);
+            expect($output)->toBe("Initialized empty Git repository in /tmp/project/.git\n");
             expect($exitCode)->toBeExitSuccess();
         }
-    )
-        ->with([
-            ['Initialized empty Git repository in /tmp/project/.git'],
-            ['Reinitialized existing Git repository in /tmp/project/.git/']
-        ]);
+    );
+
+    it(
+        'outputs reinitialize message on already .git directory',
+        function () {
+            $tester = new CommandTester(new InitCommand);
+            $tester->execute([]);
+            $tester->execute([]);
+
+            $output = $tester->getDisplay();
+            $exitCode = $tester->getStatusCode();
+
+            expect($output)->toBe("Reinitialized existing Git repository in /tmp/project/.git/\n");
+            expect($exitCode)->toBeExitSuccess();
+        }
+    );
 });
